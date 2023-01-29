dccd4e9e5bace41507b9472ca071dc46da036752
875881559@qq.com <Best12167>
2023-01-30 02:58:28 +0800

    QTWEBENGINE_CHROMIUM_FLAGS

--- bootstrapui/main.py
+++ bootstrapui/main.py
@@ -4,8 +4,10 @@ from PyQt5.QtWidgets import QApplication
 from ui import UIWindow
 from bridge import JsBridge
 import sys
+import os
 
 if __name__ == '__main__':
+    os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--foo-arg=foo-value --bar-arg=bar-value"
     app = QApplication(sys.argv)
     ui_window = UIWindow(title='qics Qt5 Demo', html="/assets/html/index.html")
     js_bridge = JsBridge(ui_window.webView)
